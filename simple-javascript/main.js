8a13e81e99c0f566ab37c241fc1f59d5bd17fb6d
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-05-31 20:48:27 +0900

    JavaScript を別ファイルから読み込むサンプルを追加

---
+++ simple-javascript/main.js
@@ -0,0 +1,8 @@
+const textInput = document.getElementById('text-input')
+const submitButton = document.getElementById('submit-button')
+
+textInput.oninput = (event) => {
+  const inputValue = event.target.value
+  // 入力欄の値が空文字などの場合はボタンは disabled=true、その他の場合は disabled=false にする
+  submitButton.disabled = !inputValue
+}
